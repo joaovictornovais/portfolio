7bace71d2536f4493d3abb81689d4a466394540c
João Victor Novais <joao.dipaay@gmail.com>
2023-09-20 14:35:47 -0300

    portfolio new ui

---
+++ src/components/Aside.jsx
@@ -0,0 +1,26 @@
+import SocialLink from "./SocialLink";
+import { links } from "../db/Links";
+
+const Aside = () => {
+  return (
+    <aside className="flex flex-col items-center gap-3 px-8">
+      <img
+        src="https://www.github.com/joaovictornovais.png"
+        className="rounded-xl sm:w-[500px] w-[240px]"
+      />
+      <span className="text-center font-semibold">Social links</span>
+      <ul className="flex flex-col">
+        {links.map((link) => (
+          <SocialLink
+            key={link.id}
+            name={link.name}
+            link={link.link}
+            color={link.color}
+          />
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Aside;
