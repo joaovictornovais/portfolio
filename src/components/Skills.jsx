33ccb43645c00641cb25102d80cbe030d05b8db0
João Victor Novais <joao.dipaay@gmail.com>
2024-02-08 11:52:58 -0300

    refactor: stacks

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,29 +1,56 @@
 const Skills = () => {
+  const stacks = [
+    {
+      id: 1,
+      name: "Java",
+      image:
+        "https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png",
+    },
+    {
+      id: 2,
+      name: "Spring",
+      image:
+        "https://user-images.githubusercontent.com/25181517/117201470-f6d56780-adec-11eb-8f7c-e70e376cfd07.png",
+    },
+    {
+      id: 3,
+      name: "JavaScript",
+      image:
+        "https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png",
+    },
+    {
+      id: 4,
+      name: "React.js",
+      image:
+        "https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png",
+    },
+    {
+      id: 5,
+      name: "Python",
+      image:
+        "https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png",
+    },
+  ];
+
   return (
     <ul className="grid grid-cols-5 gap-6">
-
-        <li className="flex flex-col items-center space-y-2">
-            <img src="https://user-images.githubusercontent.com/25181517/117201156-9a724800-adec-11eb-9a9d-3cd0f67da4bc.png" width={60} alt="Java" />
-            <p className="text-xs text-zinc-400">Java</p>
+      {stacks.map((stack) => (
+        <li
+          key={stack.id}
+          className="flex flex-col gap-3 items-center group cursor-pointer"
+        >
+          <img
+            src={stack.image}
+            alt={stack.name}
+            className="w-16 group-hover:scale-110 transition-all"
+          />
+          <p className="text-xs text-zinc-400 group-hover:text-zinc-900 transition-colors group-hover:font-semibold">
+            {stack.name}
+          </p>
         </li>
-        <li className="flex flex-col items-center space-y-2">
-            <img src="https://user-images.githubusercontent.com/25181517/117201470-f6d56780-adec-11eb-8f7c-e70e376cfd07.png" width={60} alt="Spring" />
-            <p className="text-xs text-zinc-400">Spring</p>
-        </li>
-        <li className="flex flex-col items-center space-y-2">
-            <img src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png" width={60} alt="JavaScript" />
-            <p className="text-xs text-zinc-400">JavaScript</p>
-        </li>
-        <li className="flex flex-col items-center space-y-2">
-            <img src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png" width={60} alt="React.js" />
-            <p className="text-xs text-zinc-400">React.js</p>
-        </li>
-        <li className="flex flex-col items-center space-y-2">
-            <img src="https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png" width={60} alt="Python" />
-            <p className="text-xs text-zinc-400">Python</p>
-        </li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { projects } from "../db/GithubProjects";
 
 const Home = () => {
   let aboutMe =
-    "My name is João Victor and I am a 20-year-old software engineer. In my free time, I enjoy playing Valorant and working on personal projects. I am excited to share my skills and experience with others in the field.";
+    "My name is João Victor and I am a 20-year-old software engineer. In my free time, I enjoy to play Valorant and working on personal projects. I am excited to share my skills and experience with others in the field.";
 
   let myProjects =
     "I'm a dedicated Java backend developer with a passion for solve real-life problems with code. I find immense satisfaction in using Java to create solutions that address the everyday challenges faced by people. But when no one is watching, I like to create front-end projetcs, like this one 😎";
@@ -15,16 +15,12 @@ const Home = () => {
     <main className="flex flex-col sm:flex-row gap-8 p-6">
       <Aside />
       <div className="flex flex-col gap-14">
+        <TextSection title="Hey, there! 👋" text={aboutMe} size="text-xl" />
         <TextSection
-          title="Hi, I'm João Victor! 👋"
-          text={aboutMe}
-          size="text-xl"
-        />
-        <TextSection 
           title="Main Skills 🐱‍👤"
           text={<Skills />}
           size="text-xl"
-          />
+        />
         <TextSection
           title="Main Projects 🧪"
           text={myProjects}
@@ -38,7 +34,17 @@ const Home = () => {
               description={project.description}
             />
           ))}
-          <p>You can see more projects in my <a href="https://github.com/joaovictornovais" className="underline" target="__blank">GitHub</a>!</p>
+          <p>
+            You can see more projects in my{" "}
+            <a
+              href="https://github.com/joaovictornovais"
+              className="underline"
+              target="__blank"
+            >
+              GitHub
+            </a>
+            !
+          </p>
         </ul>
       </div>
     </main>
